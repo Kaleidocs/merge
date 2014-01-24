c27ae06af1112badcfd9e18829e21c27bde421d5
Hoàng Minh Thắng <p@banphim.net>
2014-01-24 07:56:28 +0700

    first commit

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject kaleidocs/merge "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies
+  [[org.clojure/clojure "1.5.1"]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document "1.0.2"]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.docx "1.0.2"]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.odt "1.0.2"]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template "1.0.2"]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template.velocity "1.0.2"]
+   [org.clojure/clojure "1.5.1"]
+   [org.fusesource/sigar "1.6.4"]
+   [commons-io "1.4"]
+   [commons-cli "1.1"]
+   [org.json/json "20090211"]
+   [org.openoffice/unoil "3.2.1"]
+   [org.openoffice/juh "3.2.1"]])
---
+++ src/kaleidocs/merge.clj
@@ -0,0 +1,26 @@
+(ns kaleidocs.merge)
+
+(import fr.opensagres.xdocreport.document.IXDocReport)
+(import fr.opensagres.xdocreport.template.TemplateEngineKind)
+(import fr.opensagres.xdocreport.document.registry.XDocReportRegistry)
+
+(defn list-of-maps->java [coll]
+  (java.util.ArrayList.
+   (map #(java.util.HashMap. %) coll)))
+
+(defn merge-doc
+  "Merge document from template with Velocity placeholders"
+  [input-file output-file columns context-map]
+  (let [in (clojure.java.io/input-stream input-file)
+        out (clojure.java.io/output-stream output-file)
+        report (. (XDocReportRegistry/getRegistry) loadReport in TemplateEngineKind/Velocity)
+        fields-metadata (.createFieldsMetadata report)
+        context (. report createContext)]
+    (doseq [col columns]
+      (.addFieldAsList fields-metadata col))
+    (.setFieldsMetadata report fields-metadata)
+    (doseq [[k v] context-map]
+      (if (seq? v)
+        (. context (put (name k) (list-of-maps->java v)))
+        (. context (put (name k) v))))
+    (. report process context out)))
