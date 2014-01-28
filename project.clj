cdb56944bc9c359713321b3de17e24de5af86995
Hoàng Minh Thắng <p@banphim.net>
2014-01-28 18:49:42 +0700

    update deps, bump to v0.1.0

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
-(defproject kaleidocs/merge "0.1.0-SNAPSHOT"
+(defproject kaleidocs/merge "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies
   [[org.clojure/clojure "1.5.1"]
-   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document "1.0.2"]
-   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.docx "1.0.2"]
-   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.odt "1.0.2"]
-   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template "1.0.2"]
-   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template.velocity "1.0.2"]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document "1.0.3"]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.docx "1.0.3"]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.odt "1.0.3"]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.ods "1.0.3"]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template "1.0.3"]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template.velocity "1.0.3"]
    [org.clojure/clojure "1.5.1"]
    [org.fusesource/sigar "1.6.4"]
    [commons-io "1.4"]
