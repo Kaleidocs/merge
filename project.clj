aa9157c1a1b1b04a803e2106aa7497bc9368a69d
Hoàng Minh Thắng <p@banphim.net>
2014-01-29 08:53:30 +0700

    remove unused deps

--- project.clj
+++ project.clj
@@ -6,15 +6,7 @@
   :dependencies
   [[org.clojure/clojure "1.5.1"]
    [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document "1.0.3"]
-   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.docx "1.0.3"]
    [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.odt "1.0.3"]
    [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.ods "1.0.3"]
    [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template "1.0.3"]
-   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template.velocity "1.0.3"]
-   [org.clojure/clojure "1.5.1"]
-   [org.fusesource/sigar "1.6.4"]
-   [commons-io "1.4"]
-   [commons-cli "1.1"]
-   [org.json/json "20090211"]
-   [org.openoffice/unoil "3.2.1"]
-   [org.openoffice/juh "3.2.1"]])
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template.velocity "1.0.3"]])
