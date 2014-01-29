0381fdff5b3f0e30b798c1b828ae7fba947e8f7e
Hoàng Minh Thắng <p@banphim.net>
2014-01-29 17:50:35 +0700

    merge-doc now also allow three arguments (no columns specified)

--- src/kaleidocs/merge.clj
+++ src/kaleidocs/merge.clj
@@ -9,18 +9,20 @@
    (map #(java.util.HashMap. %) coll)))
 
 (defn merge-doc
-  "Merge document from template with Velocity placeholders"
-  [input-file output-file columns context-map]
-  (let [in (clojure.java.io/input-stream input-file)
-        out (clojure.java.io/output-stream output-file)
-        report (. (XDocReportRegistry/getRegistry) loadReport in TemplateEngineKind/Velocity)
-        fields-metadata (.createFieldsMetadata report)
-        context (. report createContext)]
-    (doseq [col columns]
-      (.addFieldAsList fields-metadata col))
-    (.setFieldsMetadata report fields-metadata)
-    (doseq [[k v] context-map]
-      (if (seq? v)
-        (. context (put (name k) (list-of-maps->java v)))
-        (. context (put (name k) v))))
-    (. report process context out)))
+  "Produce (aka merge) a document from an ODF template with Velocity placeholders."
+  ([input-file output-file columns context-map]
+     (let [in (clojure.java.io/input-stream input-file)
+           out (clojure.java.io/output-stream output-file)
+           report (. (XDocReportRegistry/getRegistry) loadReport in TemplateEngineKind/Velocity)
+           fields-metadata (.createFieldsMetadata report)
+           context (. report createContext)]
+       (doseq [col columns]
+         (.addFieldAsList fields-metadata col))
+       (.setFieldsMetadata report fields-metadata)
+       (doseq [[k v] context-map]
+         (if (seq? v)
+           (. context (put (name k) (list-of-maps->java v)))
+           (. context (put (name k) v))))
+       (. report process context out)))
+  ([input-file output-file context-map]
+     (merge-doc input-file output-file [] context-map)))
