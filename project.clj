891113d3e0367a4472dfaa01835de03a66d1615b
Hoàng Minh Thắng <p@banphim.net>
2014-02-08 10:42:26 +0700

    move template engines to dev dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,10 @@
   :dependencies
   [[org.clojure/clojure "1.5.1"]
    [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document "1.0.3"]
-   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.odt "1.0.3"]
-   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.ods "1.0.3"]
    [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template "1.0.3"]
-   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template.velocity "1.0.3"]])
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template.velocity "1.0.3"]]
+  :profiles
+  {:dev
+   {:dependencies
+    [[fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.odt "1.0.3"]
+     [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document.ods "1.0.3"]]}})
