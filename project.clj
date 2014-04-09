6816859e4ff2de161b7e6a115f56db7ce9764cc5
Hoàng Minh Thắng <p@banphim.net>
2014-04-09 08:52:24 +0700

    use freemarker in deps

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject kaleidocs/merge "0.3.0"
-  :description "A Clojure wrapper for XDocReport"
+(defproject kaleidocs/meld "0.4.0"
+  :description "A Clojure wrapper for XDocReport using freemarker"
   :url "https://github.com/Kaleidocs/merge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,7 +7,7 @@
   [[org.clojure/clojure "1.5.1"]
    [fr.opensagres.xdocreport/fr.opensagres.xdocreport.document "1.0.3"]
    [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template "1.0.3"]
-   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template.velocity "1.0.3"]]
+   [fr.opensagres.xdocreport/fr.opensagres.xdocreport.template.freemarker "1.0.3"]]
   :profiles
   {:dev
    {:dependencies
